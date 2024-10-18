539625136076dd86d243c51580e1490b0aefa66f
yifeichen2024 <cyf2024@outlook.com>
2024-10-17 22:23:20 -0500

    submit for lecture10

---
+++ Week4/timesteppers_lec.py
@@ -0,0 +1,132 @@
+import numpy as np
+from scipy import sparse
+import scipy.sparse.linalg as spla
+
+class Timestepper:
+
+    def __init__(self):
+        self.t = 0
+        self.iter = 0
+        self.dt = None
+
+    def step(self, dt):
+        self.u = self._step(dt)
+        self.dt = dt
+        self.t += dt
+        self.iter += 1
+
+    def evolve(self, dt, time):
+        while self.t < time - 1e-8:
+            self.step(dt)
+
+
+class ExplicitTimestepper(Timestepper):
+
+    def __init__(self, u, f):
+        super().__init__()
+        self.u = u
+        self.f = f
+
+
+class ImplicitTimestepper(Timestepper):
+
+    def __init__(self, u, L):
+        super().__init__()
+        self.u = u
+        self.L = L
+        N = len(u)
+        self.I = sparse.eye(N, N)
+
+
+class ForwardEuler(ExplicitTimestepper):
+
+    def _step(self, dt):
+        return self.u + dt*self.f(self.u)
+
+
+class LaxFriedrichs(ExplicitTimestepper):
+
+    def __init__(self, u, f):
+        super().__init__(u, f)
+        N = len(u)
+        A = sparse.diags([1/2, 1/2], offsets=[-1, 1], shape=[N, N])
+        A = A.tocsr()
+        A[0, -1] = 1/2
+        A[-1, 0] = 1/2
+        self.A = A
+
+    def _step(self, dt):
+        return self.A @ self.u + dt*self.f(self.u)
+
+
+class Leapfrog(ExplicitTimestepper):
+
+    def _step(self, dt):
+        if self.iter == 0:
+            self.u_old = np.copy(self.u)
+            return self.u + dt*self.f(self.u)
+        else:
+            u_temp = self.u_old + 2*dt*self.f(self.u)
+            self.u_old = np.copy(self.u)
+            return u_temp
+
+
+class LaxWendroff(Timestepper):
+
+    def __init__(self, u, f1, f2):
+        self.t = 0
+        self.iter = 0
+        self.u = u
+        self.f1 = f1
+        self.f2 = f2
+
+    def _step(self, dt):
+        return self.u + dt*self.f1(self.u) + dt**2/2*self.f2(self.u)
+
+
+class Multistage(ExplicitTimestepper):
+
+    def __init__(self, u, f, stages, a, b):
+        pass
+
+    def _step(self, dt):
+        pass
+
+
+class AdamsBashforth(ExplicitTimestepper):
+
+    def __init__(self, u, f, steps, dt):
+        pass
+
+    def _step(self, dt):
+        pass
+
+
+class BackwardEuler(ImplicitTimestepper):
+
+    def _step(self, dt):
+        if dt != self.dt:
+            self.LHS = self.I - dt*self.L.matrix
+            self.LU = spla.splu(self.LHS.tocsc(), permc_spec='NATURAL')
+        return self.LU.solve(self.u)
+
+
+class CrankNicolson(ImplicitTimestepper):
+
+    def _step(self, dt):
+        if dt != self.dt:
+            self.LHS = self.I - dt/2*self.L.matrix
+            self.RHS = self.I + dt/2*self.L.matrix
+            self.LU = spla.splu(self.LHS.tocsc(), permc_spec='NATURAL')
+        return self.LU.solve(self.RHS @ self.u)
+
+
+class BackwardDifferentiationFormula(ImplicitTimestepper):
+
+    def __init__(self, u, L, steps):
+        pass
+
+    def _step(self, dt):
+        pass
+
+
