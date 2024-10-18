206b45d8a7627ed36a2de423082426893fd12cce
Daniel Lecoanet <dlecoanet@gmail.com>
2024-10-18 10:29:28 -0500

    first grading for HW3

--- hw3_test2.py
+++ hw3_test2.py
@@ -6,37 +6,9 @@ import timesteppers
 
 resolution_list = [100, 200, 400]
 
-error_RK_2_2 = {100:0.5, 200:0.15, 400:0.05}
+error_AB_2_4 = {100:0.1, 200:0.05, 400:0.01}
 @pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_2_2(resolution):
-    grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
-    x = grid.values
-    IC = np.exp(-(x-np.pi)**2*8)
-
-    target = np.exp(-(x-np.pi-2*np.pi*0.2)**2*8)
-
-    d = finite.CenteredFiniteDifference(grid)
-    f = lambda u: d @ u
-
-    stages = 2
-    a = np.array([[  0,   0],
-                  [1/2,   0]])
-    b = np.array([0, 1])
-    
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
-
-    alpha = 0.5
-    num_periods = 1.8
-    ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
-
-    error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_2_2[resolution]
-
-    assert error < error_est
-
-error_RK_2_4 = {100:0.15, 200:0.05, 400:0.01}
-@pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_2_4(resolution):
+def test_AB_2_4(resolution):
     grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
     x = grid.values
     IC = np.exp(-(x-np.pi)**2*8)
@@ -46,25 +18,20 @@ def test_RK_2_4(resolution):
     d = finite.CenteredFiniteDifference4(grid)
     f = lambda u: d @ u
 
-    stages = 2
-    a = np.array([[  0,   0],
-                  [1/2,   0]])
-    b = np.array([0, 1])
+    alpha = 0.3
+    ts = timesteppers.AdamsBashforth(IC, f, 2, alpha*grid.dx)
 
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
-
-    alpha = 0.5
     num_periods = 1.2
     ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
 
     error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_2_4[resolution]
+    error_est = error_AB_2_4[resolution]
 
     assert error < error_est
 
-error_RK_3_2 = {100:0.5, 200:0.2, 400:0.05}
+error_AB_3_2 = {100:0.5, 200:0.2, 400:0.05}
 @pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_3_2(resolution):
+def test_AB_3_2(resolution):
     grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
     x = grid.values
     IC = np.exp(-(x-np.pi)**2*8)
@@ -74,109 +41,84 @@ def test_RK_3_2(resolution):
     d = finite.CenteredFiniteDifference(grid)
     f = lambda u: d @ u
 
-    stages = 3
-    a = np.array([[  0, 0, 0],
-                  [1/2, 0, 0],
-                  [ -1, 2, 0]])
-    b = np.array([1, 4, 1])/6
+    alpha = 0.3
+    ts = timesteppers.AdamsBashforth(IC, f, 2, alpha*grid.dx)
 
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
-
-    alpha = 0.5
     num_periods = 1.8
     ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
 
     error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_3_2[resolution]
+    error_est = error_AB_3_2[resolution]
 
     assert error < error_est
 
-error_RK_3_4 = {100:0.04, 200:0.005, 400:3e-4}
+
+error_AB_5_4 = {100:0.05, 200:0.003, 400:2e-4}
 @pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_3_4(resolution):
-    grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
-    x = grid.values
-    IC = np.exp(-(x-np.pi)**2*8)
-
-    target = np.exp(-(x-np.pi+2*np.pi*0.2)**2*8)
-
-    d = finite.CenteredFiniteDifference4(grid)
-    f = lambda u: d @ u
-
-    stages = 3
-    a = np.array([[  0, 0, 0],
-                  [1/2, 0, 0],
-                  [ -1, 2, 0]])
-    b = np.array([1, 4, 1])/6
-
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
-
-    alpha = 0.5
-    num_periods = 1.2
-    ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
-
-    error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_3_4[resolution]
-
-    assert error < error_est
-
-error_RK_4_2 = {100:0.5, 200:0.2, 400:0.05}
-@pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_4_2(resolution):
+def test_AB_5_4(resolution):
     grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
     x = grid.values
     IC = np.exp(-(x-np.pi)**2*8)
 
     target = np.exp(-(x-np.pi-2*np.pi*0.2)**2*8)
 
-    d = finite.CenteredFiniteDifference(grid)
+    d = finite.CenteredFiniteDifference4(grid)
     f = lambda u: d @ u
-    
-    stages = 4
-    a = np.array([[  0,   0, 0, 0],
-                  [1/2,   0, 0, 0],
-                  [  0, 1/2, 0, 0],
-                  [  0,   0, 1, 0]])
-    b = np.array([1, 2, 2, 1])/6
-    
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
 
-    alpha = 0.5
+    alpha = 0.1
+    ts = timesteppers.AdamsBashforth(IC, f, 5, alpha*grid.dx)
+
     num_periods = 1.8
     ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
 
     error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_4_2[resolution]
+    error_est = error_AB_2_4[resolution]
 
     assert error < error_est
 
-error_RK_4_4 = {100:0.04, 200:0.003, 400:2e-4}
+error_AB_6_2 = {100:0.5, 200:0.3, 400:0.06}
 @pytest.mark.parametrize('resolution', resolution_list)
-def test_RK_4_4(resolution):
+def test_AB_6_2(resolution):
     grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
     x = grid.values
-    IC = np.exp(-(x-np.pi)**2*8)
 
+    IC = np.exp(-(x-np.pi)**2*8)
+    target = np.exp(-(x-np.pi+2*np.pi*0.2)**2*8)
+
+    d = finite.CenteredFiniteDifference(grid)
+    f = lambda u: d @ u
+
+    alpha = 0.05
+    ts = timesteppers.AdamsBashforth(IC, f, 6, alpha*grid.dx)
+
+    num_periods = 1.2
+    ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
+
+    error = np.max(np.abs( ts.u - target))
+    error_est = error_AB_6_2[resolution]
+
+    assert error < error_est
+
+error_AB_6_4 = {100:0.04, 200:0.003, 400:2e-4}
+@pytest.mark.parametrize('resolution', resolution_list)
+def test_AB_6_4(resolution):
+    grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
+    x = grid.values
+
+    IC = np.exp(-(x-np.pi)**2*8)
     target = np.exp(-(x-np.pi+2*np.pi*0.2)**2*8)
 
     d = finite.CenteredFiniteDifference4(grid)
     f = lambda u: d @ u
 
-    stages = 4
-    a = np.array([[  0,   0, 0, 0],
-                  [1/2,   0, 0, 0],
-                  [  0, 1/2, 0, 0],
-                  [  0,   0, 1, 0]])
-    b = np.array([1, 2, 2, 1])/6
+    alpha = 0.05
+    ts = timesteppers.AdamsBashforth(IC, f, 6, alpha*grid.dx)
 
-    ts = timesteppers.Multistage(IC, f, stages, a, b)
-
-    alpha = 0.5
     num_periods = 1.2
     ts.evolve(alpha*grid.dx, 2*np.pi*num_periods)
 
     error = np.max(np.abs( ts.u - target))
-    error_est = error_RK_4_4[resolution]
+    error_est = error_AB_6_4[resolution]
 
     assert error < error_est
 
