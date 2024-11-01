d2e84d1d1f897c708604704906c4e89ab954b6d1
Daniel Lecoanet <dlecoanet@gmail.com>
2024-11-01 10:20:19 -0500

    first grading for HW5

--- hw5_test2.py
+++ hw5_test2.py
@@ -6,72 +6,10 @@ import equations
 
 resolution_list = [100, 200, 400]
 
-error_BDF2 = {(100,2):0.15, (200,2):0.04, (400,2):0.008, (100,4):0.07, (200,4):0.007, (400,4):0.0012}
+error_wave_variable = {(100,2):0.25, (200,2):0.25, (400,2):0.1, (100,4):0.2, (200,4):0.02, (400,4):0.002}
 @pytest.mark.parametrize('resolution', resolution_list)
 @pytest.mark.parametrize('spatial_order', [2, 4])
-def test_BDF2(resolution, spatial_order):
-    grid = finite.UniformPeriodicGrid(resolution, 5)
-    x = grid.values
-    
-    IC = 0*x
-    for i, xx in enumerate(x):
-        if xx > 1 and xx <= 2:
-            IC[i] = (xx-1)
-        elif xx > 2 and xx < 3:
-            IC[i] = (3-xx)
-    
-    d = finite.DifferenceUniformGrid(1, spatial_order, grid)
-    d2 = finite.DifferenceUniformGrid(2, spatial_order, grid)
-    nu = 1e-2
-    
-    u = IC
-    vburgers_problem = equations.ViscousBurgers(u, nu, d, d2)
-    ts = timesteppers.BDFExtrapolate(vburgers_problem, 2)
-
-    alpha = 0.5
-    dt = alpha*grid.dx
-    ts.evolve(dt, 3)
-
-    solution = np.loadtxt('solutions/u_burgers_%i.dat' %resolution)
-    error = np.max(np.abs(solution - u))
-    error_est = error_BDF2[(resolution,spatial_order)]
-    assert error < error_est
-
-error_BDF3 = {(100,2):0.15, (200,2):0.04, (400,2):0.008, (100,4):0.05, (200,4):0.004, (400,4):0.0004}
-@pytest.mark.parametrize('resolution', resolution_list)
-@pytest.mark.parametrize('spatial_order', [2, 4])
-def test_BDF3(resolution, spatial_order):
-    grid = finite.UniformPeriodicGrid(resolution, 5)
-    x = grid.values
-
-    IC = 0*x
-    for i, xx in enumerate(x):
-        if xx > 1 and xx <= 2:
-            IC[i] = (xx-1)
-        elif xx > 2 and xx < 3:
-            IC[i] = (3-xx)
-
-    d = finite.DifferenceUniformGrid(1, spatial_order, grid)
-    d2 = finite.DifferenceUniformGrid(2, spatial_order, grid)
-    nu = 1e-2
-
-    u = IC
-    vburgers_problem = equations.ViscousBurgers(u, nu, d, d2)
-    ts = timesteppers.BDFExtrapolate(vburgers_problem, 3)
-
-    alpha = 0.5
-    dt = alpha*grid.dx
-    ts.evolve(dt, 3)
-
-    solution = np.loadtxt('solutions/u_burgers_%i.dat' %resolution)
-    error = np.max(np.abs(solution - u))
-    error_est = error_BDF3[(resolution,spatial_order)]
-    assert error < error_est
-
-error_wave = {(100,2):0.1, (200,2):0.03, (400,2):0.006, (100,4):0.006, (200,4):6e-4, (400,4):8e-5}
-@pytest.mark.parametrize('resolution', resolution_list)
-@pytest.mark.parametrize('spatial_order', [2, 4])
-def test_wave(resolution, spatial_order):
+def test_wave_variable(resolution, spatial_order):
     grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
     x = grid.values
 
@@ -80,7 +18,7 @@ def test_wave(resolution, spatial_order):
 
     d = finite.DifferenceUniformGrid(1, spatial_order, grid)
 
-    rho0 = 3
+    rho0 = 1+0.8*np.sin(x+np.pi/3)
     gamma_p0 = 1
 
     soundwave_problem = equations.SoundWave(u, p, d, rho0, gamma_p0)
@@ -90,8 +28,60 @@ def test_wave(resolution, spatial_order):
     dt = alpha*grid.dx
     ts.evolve(dt, np.pi)
 
-    solution = np.loadtxt('solutions/u_c_%i.dat' %resolution)
+    solution = np.loadtxt('solutions/u_0p8_%i.dat' %resolution)
     error = np.max(np.abs(solution - u))
-    error_est = error_wave[(resolution,spatial_order)]
+    error_est = error_wave_variable[(resolution,spatial_order)]
+    assert error < error_est
+
+error_RD = {(100,2):0.02, (200,2):0.006, (400,2):0.002, (100,4):0.001, (200,4):0.001, (400,4):0.001}
+@pytest.mark.parametrize('resolution', resolution_list)
+@pytest.mark.parametrize('spatial_order', [2, 4])
+def test_reaction_diffusion(resolution, spatial_order):
+    grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
+    x = grid.values
+
+    c = 0.1*np.exp(-(x-np.pi)**2*8)
+
+    c_target = 1
+    D = 1e-3
+
+    d2 = finite.DifferenceUniformGrid(2, spatial_order, grid)
+
+    reaction_diffusion_problem = equations.ReactionDiffusion(c, d2, c_target, D)
+    ts = timesteppers.CNAB(reaction_diffusion_problem)
+
+    alpha = 0.2
+    dt = alpha*grid.dx
+    ts.evolve(dt, 10)
+
+    solution = np.loadtxt('solutions/c_const_%i.dat' %resolution)
+    error = np.max(np.abs(solution - c))
+    error_est = error_RD[(resolution,spatial_order)]
+    assert error < error_est
+
+error_RD_variable = {(100,2):0.02, (200,2):0.006, (400,2):0.003, (100,4):0.0015, (200,4):0.0015, (400,4):0.002}
+@pytest.mark.parametrize('resolution', resolution_list)
+@pytest.mark.parametrize('spatial_order', [2, 4])
+def test_reaction_diffusion_variable(resolution, spatial_order):
+    grid = finite.UniformPeriodicGrid(resolution, 2*np.pi)
+    x = grid.values
+
+    c = 0.1*np.exp(-(x-np.pi)**2*8)
+
+    c_target = 1-0.5*np.sin(3*x)
+    D = 1e-3
+
+    d2 = finite.DifferenceUniformGrid(2, spatial_order, grid)
+
+    reaction_diffusion_problem = equations.ReactionDiffusion(c, d2, c_target, D)
+    ts = timesteppers.CNAB(reaction_diffusion_problem)
+
+    alpha = 0.2
+    dt = alpha*grid.dx
+    ts.evolve(dt, 10)
+
+    solution = np.loadtxt('solutions/c_sin3x_%i.dat' %resolution)
+    error = np.max(np.abs(solution - c))
+    error_est = error_RD_variable[(resolution,spatial_order)]
     assert error < error_est
 
